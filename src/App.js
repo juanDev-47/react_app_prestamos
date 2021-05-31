18f1517b07ef0092de2fdc331533b7a94d53f83e
juanDev-47 <juanp.arenas@udea.edu.co>
2021-05-30 19:16:41 -0500

    last changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from 'react';
+import Formulario from './componentes/Formulario';
+import Header from './componentes/Header';
+import Mensaje from './componentes/Mensaje';
+import Resultado from './componentes/Resultado';
+import Spinner from './componentes/Spinner';
+
 
 function App() {
+  // definir el state
+  const [cantidad, guardarCantidad ]= useState(0);
+  const [plazo, guardarPlazo ]= useState('');
+  const [total, guardarTotal] = useState(0);
+  const [cargando, guardarCargando] = useState(false);
+
+  let componente;
+
+  if(cargando) {
+    componente = <Spinner/>
+  } else if(total === 0) {
+    componente = <Mensaje/>
+  } else {
+    componente = <Resultado
+      total={total}
+      plazo={plazo}
+      cantidad={cantidad}
+    />
+  }
+
   return (
-    <div className="App">
-      
-    </div>
+    <Fragment>
+      <Header
+        titulo="Cotizador de prestamos"
+      />
+
+      <div className="container">
+        <Formulario
+        cantidad= {cantidad}
+        guardarCantidad={guardarCantidad}
+        plazo= {plazo}
+        guardarPlazo= {guardarPlazo}
+        guardarTotal = {guardarTotal}
+        guardarCargando= {guardarCargando}
+        />
+
+        <div className="mensajes">
+          {componente} 
+        </div>  
+      </div>
+    </Fragment>
   );
 }
 
